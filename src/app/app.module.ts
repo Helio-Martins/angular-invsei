fc68608df8834e647ccb58fd0018025f5a8c9a3e
Helio-Martins <helio.martins98@hotmail.com>
2020-04-01 14:20:39 +0100

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { HomeComponent }   from './home/home.component';
+import { BooksComponent }  from './books/books.component';
+import { AuthorsComponent }      from './authors/authors.component';
+import { GenresComponent }  from './genres/genres.component';
+import { BookInstancesComponent }    from './book-instances/book-instances.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    AuthorsComponent,
+    GenresComponent,
+    BookInstancesComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
