cde154e44bd5d9ad9813db7e709cce46053cd248
Helio-Martins <helio.martins98@hotmail.com>
2020-04-01 14:22:03 +0100

    6 files added

---
+++ src/app/book-instances/book-instances.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-instances',
+  templateUrl: './book-instances.component.html',
+  styleUrls: ['./book-instances.component.css']
+})
+export class BookInstancesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
